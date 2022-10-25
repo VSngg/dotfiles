e32da54b78ae8aa515ec0ad75549ad9b90c974de
VSngg <bruh@shitposting.expert>
2022-10-25 23:29:37 +0300

    snip

--- .local/share/snip/snip_completion.bash
+++ .local/share/snip/snip_completion.bash
@@ -2,7 +2,7 @@
 
 _snip_completions(){
     findsnippets="$(find $SNIPPETS -mindepth 1 -printf '%P\n')"
-    COMPREPLY=($(compgen -W "$findsnippets" "${COMP_WORDS[1]}"))
+    COMPREPLY=($(compgen -W "$findsnippets" -- "${COMP_WORDS[1]}"))
 }
 
 complete -F _snip_completions snip
